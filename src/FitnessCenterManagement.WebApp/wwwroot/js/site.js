3b4ade72f63731608f6738ad8748fad42b9d656a
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-24 19:37:48 +0300

    Upload wwwroot images, js, css, libs

---
+++ src/FitnessCenterManagement.WebApp/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿function updateTableByPart(requestUrlPath, aspnetPartParameterName, stringPart,
+    noItemsElementText, tableElement, tableBodyElement, tableHeaderElement, colspanAmount, tableRowPrinter) {
+    var urlPath = requestUrlPath;
+    if (stringPart != '') {
+        urlPath += '?' + aspnetPartParameterName + '=';
+        urlPath += stringPart;
+    }
+
+    var table = tableElement;
+    while (table.rows.length > 1)
+        table.deleteRow(1);
+
+    $.ajax({
+        type: 'GET',
+        url: urlPath,
+        error: function (error) {
+            console.log('Error with receiving data: ' + error)
+        },
+        success: function (result) {
+            let s = '';
+            if (result.length == 0) {
+                s += '<tr>';
+                s += '<td colspan="' + colspanAmount+'" class="text-center">' + noItemsElementText + '</td>';
+                s += '</tr>';
+            }
+            else {
+                for (let el of result) {
+                    s += tableRowPrinter(el);
+                }
+            }
+            $(tableBodyElement).after(tableHeaderElement);
+            $(tableHeaderElement).after(s);
+        }
+    });
+}
