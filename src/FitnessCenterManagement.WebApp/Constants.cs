7e766a88e333c833f228cbdaf512531ec50caff1
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-24 19:38:09 +0300

    Create Constants.cs

---
+++ src/FitnessCenterManagement.WebApp/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessCenterManagement.WebApp
+{
+    public class Constants
+    {
+        internal const string UserIdClaimType = "UserId";
+        internal const decimal MinBalance = 0.0m;
+
+        public const string TrainerRole = "Trainer";
+        public const string UserRole = "User";
+        public const string ManagerRole = "Manager";
+        public const string DirectorRole = "Director";
+        public const string MarketerRole = "Marketer";
+
+    }
+}
