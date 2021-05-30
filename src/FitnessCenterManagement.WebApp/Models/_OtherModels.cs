81453bc38d67b311b3cc58d66d6ba797a5b76f2e
Vadzim Zemchonak <vzemchonak@ebsco.com>
2021-05-30 16:35:37 +0300

    Upload app resources for JQuery datatable & other

---
+++ src/FitnessCenterManagement.WebApp/Models/_OtherModels.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FitnessCenterManagement.WebApp.Models
+{
+    public enum AbonementStatus
+    {
+        Disabled = 0,
+        Enabled,
+    }
+
+    public enum AbonementCardStatus
+    {
+        Disabled = 0,
+        Enabled,
+    }
+
+    public enum DateEventStatus
+    {
+        Cancelled = 0,
+        Available,
+        Started,
+        Finished,
+    }
+    public class UserModel
+    {
+        public string Id { get; set; }
+
+        public string Surname { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+
+    public class ErrorViewModel
+    {
+        public string ErrorMessage { get; set; }
+
+        public string ErrorAttribute { get; set; }
+    }
+
+    public class LanguageModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ChangeLanguageModel
+    {
+        public string LanguageCode { get; set; }
+    }
+
+    public class ImageUploadModel
+    {
+        public int Id { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
